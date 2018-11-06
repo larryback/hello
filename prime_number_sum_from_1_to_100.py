43a21a27610a92ce8a69d139c7a87c1dbdcfc408
larryback <41230266+larryback@users.noreply.github.com>
2018-11-07 00:16:55 +0900

    prime_number_sum
    1부터 100까지의  소수의 합

--- prime_number_sum_from_1_to_100.py
+++ prime_number_sum_from_1_to_100.py
@@ -1,7 +1,20 @@
 sum = 0
-for x in range(1,101):
+for x in range(2,100):
     
-    if x/x == 1 and x%2 == 1:
-        sum = sum + x
+    if x >2 and x%2 == 0:
+        continue
+
+    elif x > 3 and x%3 == 0:
+        continue
+
+    elif x > 5 and x%5 == 0:
+        continue 
+
+    elif x > 7 and x%7 == 0:
+        continue 
+
+    else:   
+        sum = sum + x 
+        
 
 print(sum)
