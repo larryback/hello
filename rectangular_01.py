167631141a993b3d4f44366f1b589bd2ad8ee883
larryback <larryback@naver.com>
2018-11-09 16:43:04 +0900

    11월 9일 수업

--- rectangular_01.py
+++ rectangular_01.py
@@ -1,23 +1,28 @@
+cmd = input("Input(usage: base, length, width)>> ")
+print(cmd)
+cmds = cmd.split(",")
+print("{},{},{}".format(cmds[0], cmds[1], cmds[2]))
+
+
+
+
 class Quadrangle:
-    pass
+    def __init__(self, base, height, width):
+        self.base = 0
+        self.height = 0
+        self.width = 0
 
-
-
-r.input("Input(usage: length, width)>> ")
-
-p.input("Input(usage: base, height)>> ")
-
-
+ 
 
 class Rectangle(Quadrangle):
-     def __init__(self, length, width):
+    def __init__(self, length, width):
         self.length = length
         self.width = width
         r_area = length * width 
         print(r_area)
        
 
-r = Rectangle(length, width)
+        r = Rectangle(length, width)
 
 class Parallelogram(Quadrangle):
     def __init__(self, base, height):
@@ -26,11 +31,18 @@ class Parallelogram(Quadrangle):
         p_area = base * height
         print(p_area)
 
-p = Parallelogram (base, height)
+        p = Parallelogram (base, height)
 
-r.input("Input(usage: length, width)>> ")
+class Square(Quadrangle):
+    def __init__(self, length):
+        self.length = length
+        s_area = length * length
+        print(s_area)
 
-p.input("Input(usage: base, height)>> ")
+
+        s = Square (length, length)
+
+
 
 
 
@@ -41,6 +53,6 @@ p.input("Input(usage: base, height)>> ")
 
 
 
-#직사각형 = rectangle
-# 평행사변형 parallelogram
-
+# 직사각형 = rectangle
+# 평행사변형= parallelogram
+#  정사각형 = square 
---
+++ split_practice_01.py
@@ -0,0 +1,4 @@
+cmd = input("Input(usage: base, length, width)>> ")
+print(cmd)
+cmds = cdm.split(",")
+print(format(cmds[0], cmds[1], cmds[2]))  
