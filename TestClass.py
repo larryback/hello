7339495f3d336506257da49953b1b26823da3af6
larryback <larryback@naver.com>
2018-11-12 17:11:26 +0900

    11월12일 오후수업 02

--- TestClass.py
+++ TestClass.py
@@ -1,13 +1,48 @@
 class TestClass:
     name = "Test"
 
+    def __init__(self):
+        print("TTTTTTTTTTTT")
+    
+    def static_method():
+        print("STATIC")
+    @property
+    def full_name(self):
+        return self.name + "FFFFF"   
+    
     def get_name(self):
+        print("000000000000")
         return self.name
 
+    def area(self, x, y):
+        return x * y
 
 class Child(TestClass):
+    def __init__(self):
+        super().__init__()
+        print("my init!!!")
+
     def get_name(self):
+        t = super().get_name()
         return "Child Name" + self.name
+
+    def area(self, x, y):
+        t = super().area(x, y)
+        return t / 2 
+
+test = TestClass()
+child = Child()
+print("FFFFFFF>>", test.full_name)
+
+cmd = input("Input the function name>> ")
+
+getattr(test, "get name")
+getattr(TestClass, "static.method")()
+
+# print("11111>>", child.get_name())
+
+# c = callable(test.get_name)
+# print("ccccccccccc>>", c)
 
 test = TestClass() 
 print("111111>>", test.get_name())   
