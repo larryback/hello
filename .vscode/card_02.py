feac034dbf1146a81cf0292e568c3c7bf5180f4d
larryback <41230266+larryback@users.noreply.github.com>
2018-11-15 00:35:11 +0900

    11월 15일 새벽

--- .vscode/card_02.py
+++ .vscode/card_02.py
@@ -1,5 +1,13 @@
+import random
 suits = ('C', 'S', 'H', 'D')
 faces = ('A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K')
 cards = [suit+face for face in faces for suit in suits]
 print (cards)
 
+print(list(enumerate(cards)))
+
+random.shuffle(cards)
+
+print(list(cards))
+
+#print(list(enumerate(cards)))
