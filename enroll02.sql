37148b382065ac8fbbc63ba7c678626d334ad499
larryback <larryback@naver.com>
2018-12-06 15:12:38 +0900

    12/06/수정2

--- enroll02.sql
+++ enroll02.sql
@@ -1,4 +1,8 @@
+ # 각 과목에  100명의 학생을 랜덤으로 입력한다.  그래서 10번을 수행하면 학생이 1000명이 된다.
+
 insert into Enroll(subject, student)
 select sbj.id, s.id
-  from (select id from Subject where id not in (select distinct subject from Enroll) order by id limit 1) sbj, 
-       (select id from Student order by rand() limit 100) s;
+  from (select id from Subject where id not in (select distinct subject from Enroll) order by id limit 1) sbj, # 과목 중복 방지
+       (select id from Student order by rand() limit 100) s;  # 학생을 랜덤으로 100씩 생성
+       
+      
