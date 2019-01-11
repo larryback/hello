390a30b2f2b4a28c8a79b46084964777d387f081
larryback <41230266+larryback@users.noreply.github.com>
2019-01-11 20:58:36 +0900

    1월11일집

---
+++ KYOBObestsellers.py
@@ -0,0 +1,60 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = "http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf?range=1&kind=2&orderClick=DAB"
+
+# opeining up connection, grabbing the page
+
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+# html parsing
+
+page_soup = soup(page_html, "html.parser")
+
+# grabs each product
+
+#containers = page_soup.findAll("div", {"class":"detail"})
+#containers = page_soup.findAll("div", {"class":"detail"}, "div", {"class":"subtitle"})
+containers = page_soup.findAll("ul", {"id":"pro_list_type1"})
+print(containers)
+exit()
+filename = "products.csv"
+f = open(filename, "w")
+
+
+headers = "brand, product_name, shipping\n"
+
+f.write(headers)
+
+for container in containers:
+    #try:
+
+    containers = container.div.div.a.img["alt"]
+        
+    #except AttributeError:
+        #pass    
+    
+    title_container = container.findAll("a", {"class":"item-title"}) 
+    product_name = title_container[0].text
+
+    shipping_container = container.findAll("li", {"class":"price-ship"})
+    shipping = shipping_container[0].text.strip()  
+    
+    try:
+
+        print("brand: " + brand)
+
+    except NameError:
+        pass    
+    print("product_name " + product_name)
+    print("shipping " + shipping)
+
+    try:
+
+        f.write(brand + "," + product_name.replace(",", "|") + "," + shipping + "\n")
+    except NameError:
+        pass    
+
+f.close()    
---
+++ google_games.py
@@ -0,0 +1,60 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = "https://play.google.com/store/apps/category/GAME/collection/topselling_paid?hl=ko"
+
+# opeining up connection, grabbing the page
+
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+# html parsing
+
+page_soup = soup(page_html, "html.parser")
+
+# grabs each product
+
+#containers = page_soup.findAll("div", {"class":"detail"})
+containers = page_soup.findAll("div", {"class":"card-content id-track-click id-track-impression"})
+#containers = page_soup.findAll("ul", {"id":"pro_list_type1"})
+print(containers)
+exit()
+filename = "products.csv"
+f = open(filename, "w")
+
+
+headers = "brand, product_name, shipping\n"
+
+f.write(headers)
+
+for container in containers:
+    #try:
+
+    containers = container.div.div.a.img["alt"]
+        
+    #except AttributeError:
+        #pass    
+    
+    title_container = container.findAll("a", {"class":"item-title"}) 
+    product_name = title_container[0].text
+
+    shipping_container = container.findAll("li", {"class":"price-ship"})
+    shipping = shipping_container[0].text.strip()  
+    
+    try:
+
+        print("brand: " + brand)
+
+    except NameError:
+        pass    
+    print("product_name " + product_name)
+    print("shipping " + shipping)
+
+    try:
+
+        f.write(brand + "," + product_name.replace(",", "|") + "," + shipping + "\n")
+    except NameError:
+        pass    
+
+f.close()    
