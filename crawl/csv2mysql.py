a2bf5005fc06394a97d69316d7b7d6f409febb5f
larryback <larryback@naver.com>
2019-01-21 18:05:02 +0900

    1월21일수업

---
+++ crawl/csv2mysql.py
@@ -0,0 +1,35 @@
+import pymysql
+import csv
+import codecs
+
+
+def get_conn(db):
+    return pymysql.connect(
+        host='localhost',
+        user='dooo',
+        password='1234',
+        port=3306,
+        db=db,
+        charset='utf8')
+
+sql_truncate = "truncate table Meltop"
+sql_insert = "insert into Meltop(rank, title, singer, likecnt) values(%s,%s,%s,%s)"
+
+csvFile = codecs.open("./melon_top_100.csv", "r", "ms949")
+reader = csv.reader(csvFile, delimiter=',', quotechar='"')
+
+lst = []
+for row in reader:
+    lst.append([row[0] , row[1], row[2], row[3]])
+
+print("00>>", lst[0])
+print("11>>", lst[1])
+del lst[0]
+
+
+conn = get_conn('dadb')
+with conn:
+    cur = conn.cursor()
+    cur.execute(sql_truncate)
+    cur.executemany(sql_insert, lst)
+    print("Affected RowCount is", cur.rowcount)
