d4f5c1b24f20a4bbb212be2164a0f60f1ab09631
larryback <larryback@naver.com>
2019-01-25 11:40:10 +0900

    1월25일시험

--- exam-01-25/melon_top100_detail.py
+++ exam-01-25/melon_top100_detail.py
@@ -8,8 +8,7 @@ import re
 
 #url = "http://vlg.berryservice.net:8099/melon/detail?albumId=10123639"
 url = "http://vlg.berryservice.net:8099/melon/list"
-   
-
+  
 res = requests.get(url)
 html = res.text
 
@@ -18,12 +17,6 @@ trs = soup.select('div#tb_list table tbody tr[data-song-no]')
 print(len(trs))
 # print(trs[0])
 
-pattern = re.compile("\'(.*)\'")
-# pattern = re.compile('(\d)')
-def getNum(str):
-    return re.findall(pattern, str)[0]
-
-
 
 album_data = []
 
@@ -31,60 +24,17 @@ for tr in trs:
     album_json = tr.select('td:nth-of-type(4) a')
     album_title = tr.select_one('div.ellipsis.rank03 a').text
     title = tr.select_one('div.ellipsis.rank01 a').text
-    #print(title)
     
     for j in album_json:
         strings = j.attrs['href']
         pattern = re.compile("\'(.*)\'")
-        album = re.findall(pattern, strings)[0]
-        #print(album)
-        album_data.append(album)
+        album_id = re.findall(pattern, strings)[0]
+        #print(album_id)
+        album_data.append((album_id, title))
 
 
-#print(album_data)
-#exit()
-
-lst = []
-
-for album in album_data:
-
-    detail_URL = 'http://vlg.berryservice.net:8099/melon/detail?albumId='+ str(album)
-    #print("==========================================================================")
-    #print(detail_URL)
-
-    #exit()
-
-    res = requests.get(detail_URL)
-    html = res.text
-    soup = BeautifulSoup(html, "html.parser")
-    ss = soup.select('#conts > div.section_info > div')
-    #print("===================================================")
-    #print(ss)
-
-
-    for s in ss:
-
-        albums = s.select_one('div.song_name').text
-        abm = albums[4:]
-        album = abm.strip()
-        #print(album)
-        genre = s.select_one('div.meta dl dd:nth-of-type(2)').text
-        #print(genre)
-        label = s.select_one('div.meta dl dd:nth-of-type(3)').text
-        #print(label)
-        #exit()
-        rating = s.select_one('span.progress span').text
-        #print(rating)
-        lst.append((rating, label))
-
-        
-        
-
-
-
-#print(lst)
-
-#exit()
+print(album_data)
+exit()
 
 
 def get_conn(db):
@@ -96,29 +46,26 @@ def get_conn(db):
         db=db,
         charset='utf8')
 
-sql_truncate = "truncate table Album"
-sql_insert = "insert into Album(rating, label) values(%s,%s)"
+#sql_truncate = "truncate table Song"
+# sql_insert = "insert into Song(album) values(%s)"
+sql_insert = "update Song set album=%s where title=%s"
 
-
-
-#sql_insert = "update Song set genre=%s where album =%s"
-
-    
 #csvFile = codecs.open("./melon_top_100.csv", "r", "ms949")
 #reader = csv.reader(csvFile, delimiter=',', quotechar='"')
 
-print("00>>", lst[0])
-print("11>>", lst[1])
+print("00>>", album_data[0])
+print("11>>", album_data[1])
 
 conn = get_conn('melondb')
 with conn:
     cur = conn.cursor()
     #cur.execute(sql_truncate)
-    for i, data in enumerate(lst):
+    for i, data in enumerate(album_data):
         print(i, data)
         cur.execute(sql_insert, data)
     # cur.executemany(sql_insert, album_data)
     # print("Affected RowCount is", cur.rowcount)
+
 
 
 
---
+++ exam-01-25/meltop.py
@@ -0,0 +1,153 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+import json
+import csv, codecs
+import re
+
+#url = "https://www.melon.com/chart/index.htm"
+url = "http://vlg.berryservice.net:8099/melon/list"
+
+    # heads = {
+    #     "Referer": "https: // www.melon.com/chart/index.htm",
+    #     "User-Agent": "Mozilla/5.0 (Macintosh Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+    # }
+
+#res = requests.get(url, headers=heads)
+
+res = requests.get(url)
+html = res.text
+
+soup = BeautifulSoup(html, "html.parser")
+trs = soup.select('div#tb_list table tbody tr[data-song-no]')
+print(len(trs))
+#print(trs[0])
+
+
+#exit()
+
+pattern = re.compile("\'(.*)\'")
+# pattern = re.compile('(\d)')
+def getNum(str):
+    return re.findall(pattern, str)[0]
+
+
+   
+
+dic = {}   # { song_no: {title:'...', singer: '...' } }
+
+for tr in trs:
+    song_no = tr.attrs['data-song-no']
+    ranking = tr.select_one('span.rank').text
+    title = tr.select_one('div.ellipsis.rank01 a').text
+    # singers = tr.select('div.ellipsis.rank02 a')
+    singers = tr.select('div.ellipsis.rank02 span a')
+    singer = ",".join([a.text for a in singers])
+    album = getNum(tr.select_one('div.ellipsis.rank03 a').get('href'))
+    print("==========================================================================")
+    print(album)
+    exit()
+    dic[song_no] = {'ranking': int(ranking), 'title':title, 'singer': singer}
+
+    detail_URL = 'http://vlg.berryservice.net:8099/melon/detail?albumId='+ str(album)
+    print("==========================================================================")
+    print(detail_URL)
+
+    exit()
+    
+    res = requests.get(detail_URL)
+    html = res.text
+    soup = BeautifulSoup(html, "html.parser")
+    trs = soup.select('#conts > div.section_info > div')
+    print("===================================================")
+    print(trs)
+
+
+    for tr in trs:
+        albums = tr.select_one('div.song_name').text
+        abm = albums[4:]
+        album = abm.strip()
+        print(album)
+        genre = tr.select_one('div.meta dl dd:nth-of-type(2)').text
+        #print(genre)
+
+        rating = tr.select_one('span.progress span').text
+        #print(rating)
+      
+
+exit()
+
+
+
+#exit()
+print(dic)
+
+likeUrl = "https://www.melon.com/commonlike/getSongLike.json"
+likeParams = {
+    "contsIds": ",".join(dic.keys())
+}
+
+resLikecnt = requests.get(likeUrl, headers=heads, params=likeParams)
+# print(resLikecnt.url)
+jsonData = json.loads(resLikecnt.text)
+# pprint(jsonData)
+for j in jsonData['contsLike']:
+    key = str(j['CONTSID'])
+    songDic = dic[key]
+    songDic['likecnt'] = j['SUMMCNT']
+    songDics = songDic['likecnt']
+
+
+
+
+    #print("-------------------------------------------------------------------")
+    #print(a)
+    #print(j["SUMMCNT"])
+    #print(songDic["likecnt"])
+
+result = sorted(dic.items(), key=lambda d: d[1]['ranking'])
+sortLike = sorted(dic.items(), key=lambda d: d[1]['likecnt'])
+
+print("-----------------------------------------------------------")
+pprint(result[1])
+print("------------------------------------------------------------")
+pprint(sortLike[0])
+
+leastLike = sortLike[0][1]["likecnt"] 
+
+pprint(leastLike)
+
+# fp = codecs.open("./melon_top_100.csv", "r", "utf-8")
+
+# # aaa,bbb,"ccc,cc"
+# reader = csv.reader(fp, delimiter=',', quotechar='"')
+
+
+
+
+
+
+
+with codecs.open('./crawl/melon_top_100_01.csv', 'w', 'ms949') as ff:
+
+    writer = csv.writer(ff, delimiter=',', quotechar='"')
+
+    writer.writerow(['랭킹', '제목', '가수', '앨범', '좋아요', '좋아요차이'])
+    #writer.writerow
+
+
+    likesum = 0
+    diffsum = 0
+
+    for i in result:
+        song = i[1]
+        rank = song['ranking']
+        title = song['title']
+        singer = song['singer']
+        album = song['album']
+        likecnt = song['likecnt']
+        likeDiff = likecnt - leastLike
+        likesum = likesum + likecnt
+        diffsum = diffsum + likeDiff
+        l =[rank, title, singer, likecnt, likeDiff]
+        writer.writerow(l)
