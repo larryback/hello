faa7f513a9cd7819d3057f2dd81ee6cd5b343c55
larryback <41230266+larryback@users.noreply.github.com>
2019-02-05 00:23:53 +0900

    2월5일집새벽

---
+++ hello/crawl/import-module.py
@@ -0,0 +1,11 @@
+# 모듈을 읽어 들입니다.
+from math import sin, cos, tan, floor, ceil
+
+# sin, cos, tan를 구합니다.
+print("sin(1):", sin(1))
+print("cos(1):", cos(1))
+print("tan(1):", tan(1))
+
+# 내림과 올림을 구합니다.
+print("floor(2.5):", floor(2.5))
+print("ceil(2.5):", ceil(2.5))
